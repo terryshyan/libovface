ff0ffd84617fbf2d0281bf5b5a9c12366d854ffd
terryshyan <terryshyan@163.com>
2020-08-23 15:27:30 +0800

    support windows x64 compile.

--- include/ovface.h
+++ include/ovface.h
@@ -11,17 +11,23 @@
 #include <vector>
 #include <utility>
 
+#ifdef LIBOVFACE_EXPORTS
+#define OVFACE_API __declspec(dllexport)
+#else
+#define OVFACE_API
+#endif
+
 namespace ovface {
 
 #define OVFACE_VERSION "0.0.1"
 
-enum FrameFormat {
+enum OVFACE_API FrameFormat {
   FRAME_FOMAT_I420  = 0x00,
   FRAME_FOMAT_BGR   = 0x01,
   FRAME_FOMAT_RGB   = 0x02
 };
 
-enum DetectMode {
+enum OVFACE_API DetectMode {
   DETECT_MODE_VIDEO = 0x00,
   DETECT_MODE_IMAGE = 0x01
 };
@@ -31,14 +37,14 @@ enum DistaceAlgorithm {
   DISTANCE_COSINE     = 0x01
 };
 
-struct CRect {
+struct OVFACE_API CRect {
   int left;
   int top;
   int right;
   int bottom;
 };
 
-struct CResult {
+struct OVFACE_API CResult {
   CRect rect;
   std::string label;
   int identityId;
@@ -46,19 +52,19 @@ struct CResult {
   int frameId;
 };
 
-struct CIdentityParams {
+struct OVFACE_API CIdentityParams {
   std::string label;
   int identityId;
   std::vector<std::vector<float>> embeddings;
 };
 
-struct CNetWorkCPUConfig {
+struct OVFACE_API CNetWorkCPUConfig {
   int nCpuThreadsNum;         //default 0
   bool bCpuBindThread;        //default true
   int nCpuThroughputStreams;  //default 1
 };
 
-struct CVAChanParams {
+struct OVFACE_API CVAChanParams {
   std::string device;
   std::string faceDetectModelPath;
   std::string landmarksModelPath;
@@ -82,14 +88,14 @@ struct CVAChanParams {
   int fdInImgHeight;
 };
 
-struct CFrameData {
+struct OVFACE_API CFrameData {
   unsigned char *pFrame;
   int width;
   int height;
   FrameFormat format;
 };
 
-class VAChannel {
+class OVFACE_API VAChannel {
 public:
   static VAChannel *create(const CVAChanParams &params);
   static int getDefVAChanParams(CVAChanParams &params);
